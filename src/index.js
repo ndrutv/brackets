e423c0211334b73708e0a8f506b0220764e0bdd0
Andrey Fedorov <info@ndru.tv>
2023-01-31 21:16:46 +0400

    feat: update check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  
+  bracketsConfig.forEach((e, i) => {
+    arr[i] = bracketsConfig[i][0].concat(bracketsConfig[i][1]);
+  });
+  
+  for (let i = 0; i <= str.length; i++) {
+    for (let j = 0; j <= arr.length; j++) {
+      str = str.replaceAll(arr[j], '');
+
+      if (str.includes(arr[j])) {
+        j = 0;
+        i = 0;
+      }
+    }
+  }
+
+  return str.length === 0 ? true : false;
+};
